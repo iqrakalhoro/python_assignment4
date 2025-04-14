cd4280e2749a60f6f2c9a5958fde3bc354742677
iqrakalhoro <165007795+iqrakalhoro@users.noreply.github.com>
2025-04-14 07:44:12 +0500

    first commit

---
+++ lesson8.py
@@ -0,0 +1,8 @@
+# 8 MODULES IN PYTHON
+import os
+
+# Get the current working directory
+current_directory = os.getcwd()
+print(f"Current Directory: {current_directory}")
+
+# Standard Library Modules: Module: math (mathematical functions)
